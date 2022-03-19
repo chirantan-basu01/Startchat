10893a1285b3651055b4624c08111ac1a4f1fb3b
chirantan-basu01 <basuchirantan001@gmail.com>
2022-03-19 16:23:07 +0530

    login screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:startchat/screens/login_screen.dart';
+import 'package:startchat/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Startchat',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text('Hello world!'),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+      },
+      home: const LoginScreen(),
     );
   }
 }
