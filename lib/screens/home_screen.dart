c1dd8e760320880ee72806acd1f4c978a7b9b190
chirantan-basu01 <basuchirantan001@gmail.com>
2022-03-21 15:15:00 +0530

    bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
 
+import '../utils/colors.dart';
+
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.comment_bank,
+            ),
+            label: 'Meet & Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.lock_clock,
+            ),
+            label: 'Meetings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings_outlined,
+            ),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.comment_bank,
+            ),
+            label: 'Meet & Chat',
+          ),
+        ],
+      ),
     );
   }
 }
