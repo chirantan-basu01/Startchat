310487026abf84b8068c07690629b099d456938d
chirantan-basu01 <basuchirantan001@gmail.com>
2022-03-23 14:47:19 +0530

    home screen UI done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../utils/colors.dart';
+import '../widgets/home_meeting_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -20,6 +21,52 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HomeMeetingButton(
+                onPressed: () {},
+                text: 'New Meeting',
+                icon: Icons.videocam,
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                text: 'Join Meeting',
+                icon: Icons.add_box_rounded,
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                text: 'Schedule',
+                icon: Icons.calendar_today,
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                text: 'Share Screen',
+                icon: Icons.arrow_upward_rounded,
+              ),
+            ],
+          ),
+          const Expanded(
+            child: Center(
+              child: Text(
+                'Create/Join Meetings with just a click!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: footerColor,
         selectedItemColor: Colors.white,
