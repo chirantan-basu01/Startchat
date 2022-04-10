1960e850395bbb930dc56a1a79a3ccc13bd44f04
chirantan-basu01 <basuchirantan001@gmail.com>
2022-04-10 19:33:04 +0530

    room ID and user name placeholder added

--- lib/screens/video_call_screen.dart
+++ lib/screens/video_call_screen.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
 
+import '../resources/auth_methods.dart';
+import '../utils/colors.dart';
+
 class VideoCallScreen extends StatefulWidget {
-  const VideoCallScreen({ Key? key }) : super(key: key);
+  const VideoCallScreen({Key? key}) : super(key: key);
 
   @override
   State<VideoCallScreen> createState() => _VideoCallScreenState();
 }
 
 class _VideoCallScreenState extends State<VideoCallScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+  late TextEditingController meetingIdController;
+  late TextEditingController nameController;
+
+  @override
+  void initState() {
+    meetingIdController = TextEditingController();
+    nameController = TextEditingController(
+      text: _authMethods.user.displayName,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: backgroundColor,
+        title: const Text(
+          'Join a Meeting',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: meetingIdController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                fillColor: secondaryBackgroundColor,
+                filled: true,
+                border: InputBorder.none,
+                hintText: 'Room ID',
+                contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: nameController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                fillColor: secondaryBackgroundColor,
+                filled: true,
+                border: InputBorder.none,
+                hintText: 'Name',
+                contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
